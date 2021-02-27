4e37faec27c6421c1e17a4d48900092298484af5
Pradeepsingh Bhati <bh.pradeepsingh@gmail.com>
2021-02-28 03:46:23 +0530

    Add recursive approach for unbounded knapsack problem

--- Dynamic-Programming/Dynamic-Programming-master/Unbounded Knapsack/Unbounded Knapsack.cpp
+++ Dynamic-Programming/Dynamic-Programming-master/Unbounded Knapsack/Unbounded Knapsack.cpp
@@ -1,6 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class recursive_approach{
+    public:
+        int rec_helper(int curWeightSum,int i,int n,int wt[],int val[],int maxWeight,int **dp){
+            if(i==n){
+                return 0;
+            }
+            if(dp[i][curWeightSum]!=-1){
+                return dp[i][curWeightSum];
+            }
+
+            dp[i][curWeightSum] = rec_helper(curWeightSum,i+1,n,wt,val,maxWeight,dp);
+
+            if(curWeightSum+wt[i]<=maxWeight) {
+                dp[i][curWeightSum] = max(dp[i][curWeightSum],
+                                          val[i] + rec_helper(curWeightSum + wt[i], i, n, wt, val, maxWeight, dp));
+            }
+
+            return dp[i][curWeightSum];
+        }
+        int unbounded_knapsack(int n,int wt[],int val[],int maxWeight){
+            int** dp=new int*[n];
+            for(int i=0;i<n;i++){
+                dp[i]=new int[maxWeight+1];
+
+                for(int j=0;j<maxWeight+1;j++){
+                    dp[i][j]=-1;
+                }
+            }
+            return rec_helper(0,0,n,wt,val,maxWeight,dp);
+        }
+};
+
+
 class dynamic_approach{
     public:
         int unbounded_knapsack(int n,int wt[],int val[],int maxWeight){
@@ -24,5 +57,7 @@ int main(){
     int wt[2]={2,1};
     int val[2]={1,1};
     dynamic_approach dynamic;
-    cout<<dynamic.unbounded_knapsack(n,wt,val,maxWeight);
+    recursive_approach recursive;
+    cout<<dynamic.unbounded_knapsack(n,wt,val,maxWeight)<<endl;
+    cout<<recursive.unbounded_knapsack(n,wt,val,maxWeight);
 }
